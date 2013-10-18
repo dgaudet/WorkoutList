86be18db856bc1413f6c683976e5f15ab41e9ea0
dgaudet86@gmail.com <dgaudet86@gmail.com@9b412eb8-cd5c-11de-b49f-713b138df6af>
2013-10-18 04:34:24 +0000

    - Allowing ExerciseTableViewCell to show multiple rows properly - setting the height properly in ExerciseListTableViewController

--- WorkoutList/trunk/Classes/ExerciseTableViewCell.m
+++ WorkoutList/trunk/Classes/ExerciseTableViewCell.m
@@ -11,6 +11,8 @@
 
 @interface ExerciseTableViewCell (PrivateMethods)
 
++ (CGFloat)heightForString:(NSString *)string withWidth:(CGFloat)width withFont:(UIFont *)font;
+- (CGFloat)heightForCellEditingMode:(BOOL)editingMode;
 - (CGRect)_nameViewFrame;
 - (CGRect)_weightViewFrame;
 - (CGRect)_repsViewFrame;
@@ -18,6 +20,13 @@
 @end
 
 @implementation ExerciseTableViewCell
+
+#define EDITING_INSET       30.0
+#define NAME_LABEL_X        8.0
+#define WEIGHT_LABEL_X      225.0
+#define NAME_LABEL_WIDTH    WEIGHT_LABEL_X - NAME_LABEL_X
+#define NAME_LABEL_WIDTH_EDITING    WEIGHT_LABEL_X - NAME_LABEL_X - EDITING_INSET
+#define MINIMUM_HEIGHT      41.0
 
 @synthesize exercise = _exercise;
 
@@ -27,6 +36,8 @@
     if (self) {
         nameLabel = [[UILabel alloc] initWithFrame:CGRectZero];
 		nameLabel.contentMode = UIViewContentModeScaleAspectFit;
+        nameLabel.lineBreakMode = UILineBreakModeWordWrap;
+        nameLabel.numberOfLines = 999;
         nameLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:17.0];
         nameLabel.textColor = [UIColor blackColor];
         [self.contentView addSubview:nameLabel];
@@ -38,6 +49,32 @@
         [self.contentView addSubview:weightlabel];
     }
     return self;
+}
+
+#define MTVR_ROW_PADDING    6.0
+
++ (CGFloat)heightForString:(NSString *)string withWidth:(CGFloat)width withFont:(UIFont *)font {
+    CGSize size = [string sizeWithFont:font constrainedToSize:CGSizeMake(width, 485.0) lineBreakMode:NSLineBreakByWordWrapping];
+    CGFloat height = size.height;
+    if (height < MINIMUM_HEIGHT) {
+        height = MINIMUM_HEIGHT;
+    } else {
+        height = height + MTVR_ROW_PADDING;
+    }
+    return height;
+}
+
++ (CGFloat)heightForCellWithString:(NSString *)string editingMode:(BOOL)editingMode {
+    CGFloat width = NAME_LABEL_WIDTH;
+    if (editingMode) {
+        width = NAME_LABEL_WIDTH_EDITING;
+    }
+    float height = [self heightForString:string withWidth:width withFont:[UIFont fontWithName:@"Helvetica-Bold" size:17.0]];
+    return height;
+}
+
+- (CGFloat)heightForCellEditingMode:(BOOL)editingMode {
+    return [ExerciseTableViewCell heightForCellWithString:nameLabel.text editingMode:editingMode];
 }
 
 - (void)layoutSubviews {
@@ -58,25 +95,21 @@
     // Configure the view for the selected state
 }
 
-#define EDITING_INSET       30.0
-#define NAME_LABEL_X        8.0
-#define WEIGHT_LABEL_X      225.0
-
 - (CGRect)_nameViewFrame {
     if (self.editing) {
-        return CGRectMake(NAME_LABEL_X, 0.0, WEIGHT_LABEL_X - NAME_LABEL_X - EDITING_INSET, 40.0);
+        return CGRectMake(NAME_LABEL_X, 0.0, NAME_LABEL_WIDTH_EDITING, [self heightForCellEditingMode:YES]);
     }
 	else {
-        return CGRectMake(NAME_LABEL_X, 0.0, WEIGHT_LABEL_X - NAME_LABEL_X, 40.0);
+        return CGRectMake(NAME_LABEL_X, 0.0, NAME_LABEL_WIDTH, [self heightForCellEditingMode:NO]);
     }
 }
 
 - (CGRect)_weightViewFrame {
     if (self.editing) {
-        return CGRectMake(WEIGHT_LABEL_X - EDITING_INSET, 2.0, 35.0, 40.0);
+        return CGRectMake(WEIGHT_LABEL_X - EDITING_INSET, 0.0, 35.0, [self heightForCellEditingMode:YES]);
     }
 	else {
-        return CGRectMake(WEIGHT_LABEL_X, 2.0, 35.0, 40.0);
+        return CGRectMake(WEIGHT_LABEL_X, 0.0, 35.0, [self heightForCellEditingMode:NO]);
     }
 }
 
